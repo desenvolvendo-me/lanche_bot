6e158642459c3dcae84d4cb3572590e2d785b465
George Borsato <georginhoborsato@gmail.com>
2021-05-19 06:47:38 -0300

    feat/test unit lunch

--- spec/lunch/lunch_spec.rb
+++ spec/lunch/lunch_spec.rb
@@ -1,1 +1,8 @@
+require "lunch/lunch"
 
+RSpec.describe Lunch do
+  it "should create and validate object Lunch" do
+    lunch = Lunch.new("X-bugger", 10)
+    expect(lunch).to eq(lunch)
+  end
+end
