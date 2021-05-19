a7258d2fb70aa3052404a9c0aa9f0c856d3e40de
George Borsato <georginhoborsato@gmail.com>
2021-05-19 07:05:30 -0300

    refactor(test-unit-lunch) - change module

--- spec/lunch/lunch_spec.rb
+++ spec/lunch/lunch_spec.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 require "lunch/lunch"
 
 RSpec.describe Lunch do
   it "should create and validate object Lunch" do
-    lunch = Lunch.new("X-bugger", 10)
+    lunch = Lunch::Lunch.new("X-bugger", 10)
     expect(lunch).to eq(lunch)
   end
 end
