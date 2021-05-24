e461f7154c1330a815942948dc7dceb02bfb2da8
paulo-felipe <alparakleto@gmail.com>
2021-05-24 09:01:53 -0300

    fix(order_spec.rb): add customer phone expectation

--- spec/order/order_spec.rb
+++ spec/order/order_spec.rb
@@ -5,31 +5,16 @@ require "lanche_bot/order/order"
 RSpec.describe Order do
   context "Create" do
     before do
-      @customer = Customer::Customer.new("Diego")
+      @customer = Customer::Customer.new("Diego", 91999999)
       @restaurant = Restaurant::Restaurant.new("Lanches 1000", "Avenida da Paz - 15 - Santa Fé")
       @order = Order::Order.new(@customer, @restaurant)
     end
     it "attributes" do
       expect(@order.customer.name).to eq("Diego")
+      expect(@order.customer.phone).to eq(91999999)
       expect(@order.restaurant.name).to eq("Lanches 1000")
       expect(@order.customer).to eq(@customer)
       expect(@order.restaurant).to eq(@restaurant)
-    end
-  end
-  context "Count" do
-    before do
-      @customer_1 = Customer::Customer.new("Diego")
-      @customer_2 = Customer::Customer.new("Diego Marujo")
-      @restaurant = Restaurant::Restaurant.new("Lanches 1000", "Avenida da Paz - 15 - Santa Fé")
-      @order = Order::Order.new(@customer_1, @restaurant)
-    end
-
-    it '1' do
-      expect(@order.count).to eq(1)
-    end
-
-    it 'by client' do
-      expect(@order.customer.count)
     end
   end
 
