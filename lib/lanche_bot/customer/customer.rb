687fc17faee535462af3e736d867f55905180048
paulo-felipe <alparakleto@gmail.com>
2021-05-24 08:50:52 -0300

    feat(customer.rb): add get id and create csv

--- lib/lanche_bot/customer/customer.rb
+++ lib/lanche_bot/customer/customer.rb
@@ -1,12 +1,26 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 module Customer
   # classe customer
   class Customer
-    attr_accessor :name
+    DATA_PATH = "data/customers.csv"
+    attr_accessor :id, :name, :phone
 
-    def initialize(name)
+    def initialize(name, phone)
+      @id = rand(2000)
       @name = name
+      @phone = phone
+      create(self)
+    end
+
+    def create(itself)
+
+      CSV.open(DATA_PATH, "ab") do |csv|
+        csv << [self.id, self.name, self.phone]
+      end
+      self
     end
   end
 end
