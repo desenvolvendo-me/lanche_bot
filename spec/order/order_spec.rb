284eadf7134b11fd66acdf2b0e5d00e95f663044
paulo-felipe <alparakleto@gmail.com>
2021-05-24 08:49:31 -0300

    test(customer_spec.rb): add id expectation

--- spec/order/order_spec.rb
+++ spec/order/order_spec.rb
@@ -3,14 +3,34 @@
 require "lanche_bot/order/order"
 
 RSpec.describe Order do
-  it "Criar" do
-    customer = Customer::Customer.new("Diego")
-    restaurant = Restaurant::Restaurant.new("Lanches 1000", "Avenida da Paz - 15 - Santa Fé")
-    order = Order::Order.new(customer, restaurant)
-
-    expect(order.customer.name).to eq("Diego")
-    expect(order.restaurant.name).to eq("Lanches 1000")
-    expect(order.customer).to eq(customer)
-    expect(order.restaurant).to eq(restaurant)
+  context "Create" do
+    before do
+      @customer = Customer::Customer.new("Diego")
+      @restaurant = Restaurant::Restaurant.new("Lanches 1000", "Avenida da Paz - 15 - Santa Fé")
+      @order = Order::Order.new(@customer, @restaurant)
+    end
+    it "attributes" do
+      expect(@order.customer.name).to eq("Diego")
+      expect(@order.restaurant.name).to eq("Lanches 1000")
+      expect(@order.customer).to eq(@customer)
+      expect(@order.restaurant).to eq(@restaurant)
+    end
   end
+  context "Count" do
+    before do
+      @customer_1 = Customer::Customer.new("Diego")
+      @customer_2 = Customer::Customer.new("Diego Marujo")
+      @restaurant = Restaurant::Restaurant.new("Lanches 1000", "Avenida da Paz - 15 - Santa Fé")
+      @order = Order::Order.new(@customer_1, @restaurant)
+    end
+
+    it '1' do
+      expect(@order.count).to eq(1)
+    end
+
+    it 'by client' do
+      expect(@order.customer.count)
+    end
+  end
+
 end
