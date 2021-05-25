39b299b0ba4235e9f10875013c7d8fd4ae8da693
George Borsato <georginhoborsato@gmail.com>
2021-05-25 07:29:58 -0300

    chore(sale_spec.rb to offer_spec.rb) change name file

--- spec/offer/offer_spec.rb
+++ spec/offer/offer_spec.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
-require "lanche_bot/sale/sale"
+require "lanche_bot/offer/offer"
 
-RSpec.describe "sale" do
+RSpec.describe "offer" do
   context "when the object is valid" do
     let(:menu_main) { Menu::MenuMain.new("x-tudo", "um lanche muito bom", 15.0) }
     let(:menu_juice) { Menu::MenuJuice.new("manga", "300ml", 12.00) }
     let(:combo) { Combo::Combo.new(menu_main, menu_juice) }
     let(:customer) { Customer::Customer.new("George Lemos") }
     let(:lunch) { Lunch::Lunch.new("x-tudo", 12.00) }
-    let(:sale) { Sale::Sale.new(combo, customer, lunch, menu_juice, 20) }
+    let(:offer) { Offer::Offer.new(combo, customer, lunch, menu_juice, 20) }
 
     it "should validate object created" do
-      expect(sale).to eq(sale)
-      expect(sale.combo).to eq(combo)
-      expect(sale.customer).to eq(customer)
-      expect(sale.lunch).to eq(lunch)
-      expect(sale.quantity).to eq(20)
+      expect(offer).to eq(offer)
+      expect(offer.combo).to eq(combo)
+      expect(offer.customer).to eq(customer)
+      expect(offer.lunch).to eq(lunch)
+      expect(offer.quantity).to eq(20)
     end
   end
 end
