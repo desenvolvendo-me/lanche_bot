1c36fba2e0028aab173c2a89530d2cb82fc4c659
George Borsato <georginhoborsato@gmail.com>
2021-05-25 08:09:36 -0300

    chore(offer_spec.rb) remove attribute customer

--- spec/offer/offer_spec.rb
+++ spec/offer/offer_spec.rb
@@ -7,14 +7,12 @@ RSpec.describe "offer" do
     let(:menu_main) { Menu::MenuMain.new("x-tudo", "um lanche muito bom", 15.0) }
     let(:menu_juice) { Menu::MenuJuice.new("manga", "300ml", 12.00) }
     let(:combo) { Combo::Combo.new(menu_main, menu_juice) }
-    let(:customer) { Customer::Customer.new("George Lemos") }
     let(:lunch) { Lunch::Lunch.new("x-tudo", 12.00) }
-    let(:offer) { Offer::Offer.new(combo, customer, lunch, menu_juice, 20) }
+    let(:offer) { Offer::Offer.new(combo, lunch, menu_juice, 20) }
 
     it "should validate object created" do
       expect(offer).to eq(offer)
       expect(offer.combo).to eq(combo)
-      expect(offer.customer).to eq(customer)
       expect(offer.lunch).to eq(lunch)
       expect(offer.quantity).to eq(20)
     end
