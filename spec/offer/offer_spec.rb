278cdae1b5789174939c669e54c5c5c67a6f1b66
George Borsato <georginhoborsato@gmail.com>
2021-05-26 23:04:24 -0300

    feat(offer_spec.rb) include test unit discount offer

--- spec/offer/offer_spec.rb
+++ spec/offer/offer_spec.rb
@@ -16,5 +16,20 @@ RSpec.describe "offer" do
       expect(offer.lunch).to eq(lunch)
       expect(offer.quantity).to eq(20)
     end
+    it "should return discount valid" do
+      expect(offer.discount).to eq("Direito a desconto")
+    end
+  end
+
+  context "when discount not valid" do
+    let(:menu_main) { Menu::MenuMain.new("x-tudo", "um lanche muito bom", 15.0) }
+    let(:menu_juice) { Menu::MenuJuice.new("manga", "300ml", 12.00) }
+    let(:combo) { Combo::Combo.new(menu_main, menu_juice) }
+    let(:lunch) { Lunch::Lunch.new("x-tudo", 12.00) }
+    let(:offer) { Offer::Offer.new(combo, lunch, menu_juice, 0) }
+
+    it "should return not discount" do
+      expect(offer.discount).to eq("Não tem direito a desconto")
+    end
   end
 end
