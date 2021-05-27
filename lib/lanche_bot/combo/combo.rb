04e1a79832f8dbc0f78b5af584fa648bc619f307
paulo-felipe <alparakleto@gmail.com>
2021-05-27 08:39:34 -0300

    feat(combo_spec.rb): show free item name

--- lib/lanche_bot/combo/combo.rb
+++ lib/lanche_bot/combo/combo.rb
@@ -6,11 +6,18 @@ require "lanche_bot/menu/menu_juice"
 module Combo
   # combo
   class Combo
-    attr_accessor :menu_main, :menu_juice
+    attr_accessor :menu_main, :menu_juice, :free
 
-    def initialize(menu_main, menu_juice)
+    def initialize(menu_main, menu_juice, free = nil)
       @menu_main = menu_main
       @menu_juice = menu_juice
+      @free = free
+    end
+
+    def free_item
+      return "There is no free item marked yet." if @free.nil?
+      item = [@menu_main, @menu_juice][@free - 1]
+      item.name
     end
   end
 end
