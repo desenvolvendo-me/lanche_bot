b2ceb6a7491a83ffbff511b5b4f1cbb501f777e8
Lukas Pol <lukaspaespol@gmail.com>
2021-06-01 09:45:15 -0300

    feat(helpers): created methods csv_parse and csv_include to csv

---
+++ lib/lanche_bot/helpers.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "csv"
+
+class Helpers
+  def self.csv_parse(file)
+    CSV.read(file, options)
+  end
+
+  def self.csv_include(file, attributes)
+    CSV.open(file, "a", options) do |csv|
+      csv << attributes
+    end
+  end
+
+  private
+
+  def self.options
+    { col_sep: ",", headers: true }
+  end
+end
