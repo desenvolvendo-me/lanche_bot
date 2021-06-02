e87180c96ef15b2a015b3d370d221ad71c1942cf
Lukas Pol <lukaspaespol@gmail.com>
2021-06-02 07:34:29 -0300

    test(restaurant): verify if is create in csv

--- spec/restaurant/restaurant_spec.rb
+++ spec/restaurant/restaurant_spec.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
-require "lanche_bot/restaurant/restaurant"
+RSpec.describe "Restaurant" do
+  context "show" do
+    let(:restaurants) { Restaurant::Restaurant.restaurants }
 
-RSpec.describe "Lanchonete" do
+    it "check information first restaurant" do
+      restaurant = restaurants.first
+
+      expect(restaurant.name).to eq("Godzilla")
+      expect(restaurant.address).to eq("Rua do Divina Providência, nº 1234")
+      expect(restaurant.open).to eq(true)
+    end
+  end
+
   context "create" do
     before do
       @name = "Godzilla"
