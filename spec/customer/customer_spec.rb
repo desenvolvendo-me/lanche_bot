32171533f80f3efc22d43a7aca3d57b7646f3c35
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2021-06-02 20:19:24 -0300

    test(spec): added test to find customer on customer_spec.rb

--- spec/customer/customer_spec.rb
+++ spec/customer/customer_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe "customer" do
     expect(customer.phone).to eq(phone)
     expect(customer.id).to be_truthy
   end
+
+  it "get custormer with id = 3" do
+    customer = Customer::Customer.find("3")
+
+    expect(customer["name"]).to eq("Marcos")
+  end
 end
