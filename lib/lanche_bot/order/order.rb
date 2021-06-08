7de8d0009a35373b24bccb303a604a6fa28ef527
Lukas Pol <lukaspaespol@gmail.com>
2021-06-08 06:45:18 -0300

    refactor(order): remove attributes and call the method

--- lib/lanche_bot/order/order.rb
+++ lib/lanche_bot/order/order.rb
@@ -9,16 +9,14 @@ module Order
     def initialize(customer, restaurant, items = [])
       @id = rand(2000)
       @customer = customer
-      @customer_name = customer_detail
       @restaurant = restaurant
-      @restaurant_name = restaurant_detail
       @items = items
     end
 
     def create
       errors = validate_fields
       if errors.empty?
-        attributes = [id, @customer_name, @restaurant_name, items]
+        attributes = [id, customer_detail, restaurant_detail, items]
         Helpers.csv_include(DATA_PATH, attributes)
         self
       else
