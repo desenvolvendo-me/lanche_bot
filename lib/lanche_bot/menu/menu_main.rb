f61a71620e531be49e9e8a5ca917d2c1fba9004d
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2021-06-11 07:45:13 -0300

    feat(lib/lanche_bot): change initialize and added method create on menu_main.rb

--- lib/lanche_bot/menu/menu_main.rb
+++ lib/lanche_bot/menu/menu_main.rb
@@ -6,10 +6,29 @@ module Menu
     attr_accessor :price
     attr_reader :name, :description, :id
 
-    def initialize(name, description, price)
-      @name = name
-      @description = description
-      @price = price
+    DATA_PATH = "data/menu_mains.csv"
+
+    def initialize(args)
+      @name = args.fetch(:name)
+      @description = args.fetch(:description)
+      @price = args.fetch(:price)
+      @id = rand(2000)
+    end
+
+    def create
+      CSV.open(DATA_PATH, "ab") do |csv|
+        csv << [id, name, description, price]
+      end
+      self
+    end
+
+    def self.print_menu
+      menu = Helpers.csv_parse(DATA_PATH)
+      menu.map do |item|
+        p "*****MENU****"
+        p "#{item["id"]} - #{item["name"]} - #{item["description"]} - R$ #{item["price"]}"
+        MenuMain.new({ name: item["name"], description: item["description"], price: item["price"] })
+      end
     end
   end
 end
