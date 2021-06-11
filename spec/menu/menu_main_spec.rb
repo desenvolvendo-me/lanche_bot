5609c2e65a0b97efa6346a5c1fdac41438fe1468
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2021-06-11 07:46:44 -0300

    feat(spec): added before do restar csv on menu_main_spec.rb

--- spec/menu/menu_main_spec.rb
+++ spec/menu/menu_main_spec.rb
@@ -3,9 +3,24 @@
 require "lanche_bot/menu/menu_main"
 
 RSpec.describe Menu::MenuMain do
+  csv_path = "spec/fixtures/menu_mains-test.csv"
+  header = %w[id name description price]
+
+  before do
+    stub_const("Menu::MenuMain::DATA_PATH", csv_path)
+  end
+
+  before(:each) do
+    restart_csv(csv_path, header)
+    @name = "Frango xadrez"
+    @description = "frango delicioso"
+    @price = "12.40"
+    @menu_main = Menu::MenuMain.new({name: @name, description: @description, price: @price}).create
+  end
+
   context "when the object is valid" do
     it "should validate object created" do
-      menu_main = Menu::MenuMain.new("x-tudo", "um lanche muito bom", 15.0)
+      menu_main = Menu::MenuMain.new({name: "x-tudo", description: "um lanche muito bom", price: 15.0}).create
       expect(menu_main).to eq(menu_main)
     end
   end
