83557f5f327714a9eac709f7c609334d0b6e9218
paulo-felipe <alparakleto@gmail.com>
2021-06-19 21:36:41 -0300

    fix(routes.rb): rubocop quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'welcome#welcome'
+  root "welcome#welcome"
   resources :restaurants
   resources :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
