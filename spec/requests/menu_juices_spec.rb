fd3c993ed1f64263938bf07acdb1e82c072620b3
paulo-felipe <alparakleto@gmail.com>
2021-06-23 09:51:17 -0300

    test(menu_juice_spec.rb): Add request test

--- spec/requests/menu_juices_spec.rb
+++ spec/requests/menu_juices_spec.rb
@@ -1,8 +1,20 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe "MenuJuices", type: :request do
+  # MenuJuice. As you add validations to Customer, be sure to
+  # adjust the attributes here as well.
+  let(:valid_attributes) do
+    skip("Add a hash of attributes valid for your model")
+  end
+
+  let(:invalid_attributes) do
+    skip("Add a hash of attributes invalid for your model")
+  end
   describe "GET /index" do
     it "returns http success" do
+      MenuJuice.create! valid_attributes
       get "/menu_juices/index"
       expect(response).to have_http_status(:success)
     end
@@ -10,34 +22,40 @@ RSpec.describe "MenuJuices", type: :request do
 
   describe "GET /show" do
     it "returns http success" do
+      MenuJuice.create! valid_attributes
       get "/menu_juices/show"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/menu_juices/new"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/menu_juices/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do
+    context "valid" do
+      it "creates a new MenuJuice" do
+        expect do
+          post "menu_juices/create", params: { menu_juice: valid_attributes }
+        end.to change(MenuJuice, :count).by(1)
+      end
     end
   end
 
   describe "GET /edit" do
     it "returns http success" do
-      get "/menu_juices/edit"
+      MenuJuice.create! valid_attributes
+      patch "/menu_juices/edit"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /update" do
     it "returns http success" do
+      MenuJuice.create! valid_attributes
       get "/menu_juices/update"
       expect(response).to have_http_status(:success)
     end
@@ -45,9 +63,9 @@ RSpec.describe "MenuJuices", type: :request do
 
   describe "GET /destroy" do
     it "returns http success" do
+      MenuJuice.create! valid_attributes
       get "/menu_juices/destroy"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
