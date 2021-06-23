3ed946018cdd52773b7458782d4544be99927650
George Borsato <georginhoborsato@gmail.com>
2021-06-23 07:08:16 -0300

    feat(schema.db) add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_224802) do
+ActiveRecord::Schema.define(version: 2021_06_23_095001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2021_06_22_224802) do
   create_table "menu_mains", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "menu_sodas", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
